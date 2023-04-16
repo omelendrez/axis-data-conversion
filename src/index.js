25f036c0cf48ea103a73c34ce0b8507e497b567b
Omar Melendrez <omar.melendrez@gmail.com>
2023-04-16 12:00:43 -0300

    Improve file naming

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
-const schemas = require('./schema/schemas.json')
-const filler = require('./schema/post-data-conversion-data.json')
+const migrationSchemas = require('./schema/migration.json')
+const conversionSchemas = require('./schema/conversion.json')
 
-const data = require('./process/data-conversion')
-const post = require('./process/post-data-conversion')
+const data = require('./process/migration')
+const post = require('./process/conversion')
 
 async function run() {
   try {
@@ -11,65 +11,65 @@ async function run() {
     console.log('AXIS DATA CONVERSION: Starting')
     console.log()
 
-    let totalRecords = 0
-    let processed = 0
+    let readRecords = 0
+    let insertedRecords = 0
     let tables = 0
     let updatedRecords = 0
 
     await Promise.all(
-      schemas
+      migrationSchemas
         .filter((t) => !t.isDone)
         .map(async (t) => {
           const [read, inserted] = await data.convert(t)
-          totalRecords += read
-          processed += inserted
+          readRecords += read
+          insertedRecords += inserted
           tables++
         })
     )
 
     await Promise.all(
-      filler
+      conversionSchemas
         .filter((t) => !t.isDone && t.isSecure)
         .map(async (t) => {
           const [updated] = await post.secureUserPasswords(t)
-          processed += updated
-          tables++
+          updatedRecords += updated
         })
     )
 
     await Promise.all(
-      filler
+      conversionSchemas
         .filter((t) => !t.isDone && t.isInsert)
         .map(async (t) => {
           const [read, inserted] = await post.addTracking(t)
-          totalRecords += read
-          processed += inserted
-        })
-    )
-
-    await Promise.all(
-      filler
-        .filter((t) => !t.isDone && !t.isUpdate && !t.isInsert && !t.isSecure)
-        .map(async (t) => {
-          const [inserted] = await post.execute(t)
-          processed += inserted
+          readRecords += read
+          insertedRecords += inserted
           tables++
         })
     )
 
     await Promise.all(
-      filler
+      conversionSchemas
+        .filter((t) => !t.isDone && !t.isUpdate && !t.isInsert && !t.isSecure)
+        .map(async (t) => {
+          const [inserted] = await post.execute(t)
+          insertedRecords += inserted
+          tables++
+        })
+    )
+
+    await Promise.all(
+      conversionSchemas
         .filter((t) => !t.isDone && t.isUpdate)
         .map(async (t) => {
-          const [inserted] = await post.executeProcedure(t)
-          processed += inserted
+          const [updatedRows] = await post.executeProcedure(t)
+          updatedRecords += updatedRows
         })
     )
 
     console.log('Total tables processed:', tables)
-    console.log('Total records read from MSSQL:', totalRecords)
-    console.log('Total records inserted to MySQL:', processed)
-    console.log('Total records updated MySQL:', updatedRecords)
+    console.log('Total records read from MSSQL:', readRecords)
+    console.log('Total records inserted to MySQL:', insertedRecords)
+    console.log('Total records updated in MySQL:', updatedRecords)
     console.log()
     console.log('AXIS DATA CONVERSION: Completed')
 
