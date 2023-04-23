3d98f274f4c34e2989ec9114b5a70b6e9fdf24bb
Omar Melendrez <omar.melendrez@gmail.com>
2023-04-23 11:26:13 -0300

    Add port to mysql connection

--- src/mysql/mysql-connect.js
+++ src/mysql/mysql-connect.js
@@ -1,9 +1,18 @@
 const assert = require('assert').strict
 const mysql = require('mysql2/promise')
 
-assert.ok(process.env.MYSQL_SERVER, 'The "MYSQL_SERVER" environment variable is required')
-assert.ok(process.env.MYSQL_USER, 'The "MYSQL_USER" environment variable is required')
-assert.ok(process.env.MYSQL_PASSWORD, 'The "MYSQL_PASSWORD" environment variable is required')
+assert.ok(
+  process.env.MYSQL_SERVER,
+  'The "MYSQL_SERVER" environment variable is required'
+)
+assert.ok(
+  process.env.MYSQL_USER,
+  'The "MYSQL_USER" environment variable is required'
+)
+assert.ok(
+  process.env.MYSQL_PASSWORD,
+  'The "MYSQL_PASSWORD" environment variable is required'
+)
 
 const connect = async () => {
   try {
@@ -11,6 +20,7 @@ const connect = async () => {
       host: process.env.MYSQL_SERVER,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
+      port: process.env.MYSQL_PORT,
       waitForConnections: true,
       connectionLimit: 10,
       maxIdle: 10,
