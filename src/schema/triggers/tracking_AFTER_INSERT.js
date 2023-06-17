23e073a9d2efdd5271459b98b2416bfda7af9018
Omar Melendrez <omar.melendrez@gmail.com>
2023-06-17 18:00:00 -0300

    Use last status in traking list after inserting a new one (i.e.: 13)

--- src/schema/triggers/tracking_AFTER_INSERT.js
+++ src/schema/triggers/tracking_AFTER_INSERT.js
@@ -1,10 +1,21 @@
 module.exports.query = `CREATE TRIGGER tracking_AFTER_INSERT AFTER INSERT ON tracking FOR EACH ROW BEGIN
 
-  UPDATE
-    training
-  SET
-    status = NEW.status
-  WHERE
-    id = NEW.training;
+DECLARE NEW_STATUS SMALLINT;
+
+SELECT
+  MAX(status)
+INTO
+  NEW_STATUS
+FROM
+  tracking
+WHERE
+  training = NEW.training;
+
+UPDATE
+  training
+SET
+  status = NEW_STATUS
+WHERE
+  id = NEW.training;
 
 END`
