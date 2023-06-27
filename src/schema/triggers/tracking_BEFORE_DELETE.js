be3e18828eb67807fa2caccd4f9f652c91c9d0b6
Omar Melendrez <omar.melendrez@gmail.com>
2023-06-27 12:19:23 -0300

    Remove training_attendance records if tc approval is removed

--- src/schema/triggers/tracking_BEFORE_DELETE.js
+++ src/schema/triggers/tracking_BEFORE_DELETE.js
@@ -6,6 +6,10 @@ BEFORE
 
   DECLARE training_status TINYINT;
 
+  DECLARE fromDate DATE;
+  DECLARE	toDate DATE;
+  DECLARE curDate DATE;
+
   SELECT
     OLD.training, OLD.status
   INTO
@@ -18,6 +22,13 @@ BEFORE
       training = training_id;
   END IF;
 
+  IF training_status = 4 THEN
+    DELETE FROM
+      training_attendance
+    WHERE
+      training = training_id;
+  END IF;
+
   IF training_status = 5 THEN
     DELETE FROM
       training_assesment
@@ -25,7 +36,7 @@ BEFORE
       training = training_id;
   END IF;
 
-  IF training_status <= 7 THEN
+  IF training_status IN (3,4,5,6,7) THEN
     UPDATE
       training
     SET
--- src/schema/triggers/training_AFTER_UPDATE.js
+++ src/schema/triggers/training_AFTER_UPDATE.js
@@ -1,88 +1,87 @@
 module.exports.query = `CREATE TRIGGER training_AFTER_UPDATE
 AFTER
 UPDATE
-	ON training FOR EACH ROW BEGIN DECLARE learners_count SMALLINT default 0;
+	ON training FOR EACH ROW BEGIN
+
+DECLARE learners_count SMALLINT default 0;
 
 DECLARE classroom_id INT default 0;
 
-DECLARE class_records_found SMALLINT default 0;
+DECLARE classroom_records_count SMALLINT default 0;
 
-IF NEW.course <> OLD.course
-OR NEW.start <> OLD.start THEN
-SELECT
-	COUNT(1) INTO class_records_found
-FROM
-	classroom
-WHERE
-	course = NEW.course
+IF NEW.course <> OLD.course OR NEW.start <> OLD.start THEN
+
+	SELECT
+		COUNT(1) INTO classroom_records_count
+	FROM
+		classroom
+	WHERE
+		course = NEW.course
 	AND start = NEW.start;
 
-IF class_records_found = 0 THEN
-INSERT INTO
-	classroom (course, start, learners)
-VALUES
-	(NEW.course, NEW.start, 0);
+	IF classroom_records_count = 0 THEN
+		INSERT INTO
+			classroom (course, start, learners)
+		VALUES
+			(NEW.course, NEW.start, 0);
+	END IF;
 
-END IF;
+	-- New data
+	SELECT
+		id INTO classroom_id
+	FROM
+		classroom
+	WHERE
+		course = NEW.course
+		AND start = NEW.start;
 
--- New data
-SELECT
-	id INTO classroom_id
-FROM
-	classroom
-WHERE
-	course = NEW.course
-	AND start = NEW.start;
+	SELECT
+		COUNT(1) INTO learners_count
+	FROM
+		training t
+		INNER JOIN classroom c ON t.course = c.course
+		AND t.start = c.start
+	WHERE
+		c.id = classroom_id;
 
-SELECT
-	COUNT(1) INTO learners_count
-FROM
-	training t
-	INNER JOIN classroom c ON t.course = c.course
-	AND t.start = c.start
-WHERE
-	c.id = classroom_id;
+	UPDATE
+		classroom
+	SET
+		learners = learners_count
+	WHERE
+		id = classroom_id;
 
-UPDATE
-	classroom
-SET
-	learners = learners_count
-WHERE
-	id = classroom_id;
+	-- Old data
+	SELECT
+		id INTO classroom_id
+	FROM
+		classroom
+	WHERE
+		course = OLD.course
+		AND start = OLD.start;
 
--- Old data
-SELECT
-	id INTO classroom_id
-FROM
-	classroom
-WHERE
-	course = OLD.course
-	AND start = OLD.start;
+	SELECT
+		COUNT(1) INTO learners_count
+	FROM
+		training t
+		INNER JOIN classroom c ON t.course = c.course
+		AND t.start = c.start
+	WHERE
+		c.id = classroom_id;
 
-SELECT
-	COUNT(1) INTO learners_count
-FROM
-	training t
-	INNER JOIN classroom c ON t.course = c.course
-	AND t.start = c.start
-WHERE
-	c.id = classroom_id;
-
-IF learners_count = 0 THEN
-DELETE FROM
-	classroom
-WHERE
-	id = classroom_id;
-
-ELSE
-UPDATE
-	classroom
-SET
-	learners = learners_count
-WHERE
-	id = classroom_id;
-
-END IF;
+	IF learners_count = 0 THEN
+		DELETE FROM
+			classroom
+		WHERE
+			id = classroom_id;
+	ELSE
+		UPDATE
+			classroom
+		SET
+			learners = learners_count
+		WHERE
+			id = classroom_id;
+	END IF;
 
 END IF;
 
