60887ec0b436bb48b50ddd5b9d74fb9d43627ceb
Omar Melendrez <omar.melendrez@gmail.com>
2023-10-19 11:08:28 -0300

    Fix trigger that tried to update status in training record with last tracking status, after deleting all trackings

--- src/schema/triggers/training_tracking_AFTER_DELETE.js
+++ src/schema/triggers/training_tracking_AFTER_DELETE.js
@@ -2,20 +2,25 @@ module.exports.query = `CREATE TRIGGER tracking_AFTER_DELETE AFTER DELETE ON tra
 
   DECLARE v_new_status TINYINT;
 
-  SELECT
-    MAX(status)
-  INTO
-    v_new_status
-  FROM
-    training_tracking
-  WHERE
-    training = OLD.training;
+  IF EXISTS (SELECT 1 FROM training_tracking WHERE training = OLD.training)
+  THEN
 
-  UPDATE
-    training
-  SET
-    status = CASE WHEN v_new_status IS NULL THEN 0 ELSE v_new_status END
-  WHERE
-    id = OLD.training;
+    SELECT
+      MAX(status)
+    INTO
+      v_new_status
+    FROM
+      training_tracking
+    WHERE
+      training = OLD.training;
+
+    UPDATE
+      training
+    SET
+      status = CASE WHEN v_new_status IS NULL THEN 0 ELSE v_new_status END
+    WHERE
+      id = OLD.training;
+
+  END IF;
 
 END`
