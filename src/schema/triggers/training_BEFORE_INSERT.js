75f26730b905c5a0cf31929e38e2e30b7709f385
Omar Melendrez <omar.melendrez@gmail.com>
2023-10-23 12:37:08 -0300

    Implement FOET certificate expiry date calculation

--- src/schema/triggers/training_BEFORE_INSERT.js
+++ src/schema/triggers/training_BEFORE_INSERT.js
@@ -2,7 +2,9 @@ module.exports.query = `CREATE TRIGGER training_BEFORE_INSERT BEFORE INSERT ON t
 	DECLARE v_start DATE;
     DECLARE v_end DATE;
     DECLARE v_issued DATE;
+    DECLARE v_prev_expiry DATE;
     DECLARE v_expiry DATE;
+    DECLARE v_days SMALLINT;
 
     DECLARE v_duration TINYINT;
     DECLARE v_validity TINYINT;
@@ -10,15 +12,16 @@ module.exports.query = `CREATE TRIGGER training_BEFORE_INSERT BEFORE INSERT ON t
     DECLARE v_expiry_type TINYINT;
 
     SET v_start = NEW.start;
-    SET v_expiry = NEW.expiry;
+    SET v_prev_expiry = NEW.prev_expiry;
 
     SELECT duration, validity, expiry_type
     INTO v_duration, v_validity, v_expiry_type
     FROM course
     WHERE id = NEW.course;
 
-    SET v_issued = DATE_ADD(v_start, INTERVAL v_duration DAY);
-    SET v_end = DATE_ADD(v_issued, INTERVAL -1 DAY);
+    SET v_end = DATE_ADD(v_start, INTERVAL v_duration-1 DAY);
+
+    SET v_issued = v_end;
 
     IF v_expiry_type = 0 -- No expiration date
     THEN
@@ -28,6 +31,17 @@ module.exports.query = `CREATE TRIGGER training_BEFORE_INSERT BEFORE INSERT ON t
     IF v_expiry_type = 1 -- Automatic
     THEN
         SET v_expiry = DATE_ADD(DATE_ADD(v_issued, INTERVAL v_validity YEAR), INTERVAL -1 DAY);
+    END IF;
+
+    IF v_expiry_type = 2 -- FOET
+    THEN
+        SET v_days = DATEDIFF(v_prev_expiry, v_issued);
+        IF v_days BETWEEN 0 AND 60
+        THEN
+            SET v_expiry = DATE_ADD(DATE_ADD(v_prev_expiry, INTERVAL v_validity YEAR), INTERVAL -1 DAY);
+        ELSE
+            SET v_expiry = DATE_ADD(DATE_ADD(v_issued, INTERVAL v_validity YEAR), INTERVAL -1 DAY);
+        END IF;
     END IF;
 
     SET NEW.end = v_end;
